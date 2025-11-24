d9ad8aa8f77839f1b3aee094ddd83ee6d60f0060
Ratnavo Dutta <networkcode.rd@gmail.com>
2025-11-24 11:08:49 +0530

    python name generator Commit

---
+++ name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Name Generator!")
+
+city = input("Which city did you grow up in?\n")
+nickname = input ("Give us a nickname that you would like to set in name generator.\n")
+print (city + " " + nickname)
